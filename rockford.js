f3ce59dce475d6cd0936b09ea19ea6c03b81cb0c
Joe Crick <joseph.crick@blacklane.com>
2017-06-14 10:54:08 +0200

    Use map composition - more readable

--- rockford.js
+++ rockford.js
@@ -6,17 +6,20 @@ const getConfig = require('./lib/config-reader')
 const sanityStateFormatter = require('./lib/sanity-state')
 const calculateFileCoverage = require('./lib/file-coverage-calc')
 const formatDisplay = require('./lib/display-formatter')
+const Option = require('giftbox').Option;
 const config = getConfig()
-const compose = require('ramda/src/compose')
 const sanityState = sanityStateFormatter(config.sanityLevel)
-const calculateCoverageMetrics = compose(writeReport, recordCoverageTotals, calculateTotalCoverage, formatCoverageMetrics, analyzeCoverageMetrics)
 
 /**
  * Rockford - Runs a code analysis on a set of JS files to determine the DbC code coverage
  * based on simple-assertion
  */
 function rockford () {
-  calculateCoverageMetrics()
+  analyzeCoverageMetrics()
+    .map(formatCoverageMetrics)
+    .map(calculateTotalCoverage)
+    .map(recordCoverageTotals)
+    .map(writeReport)
 }
 
 /**
@@ -28,14 +31,13 @@ function analyzeCoverageMetrics () {
     functionCount: 0,
     dbcAssertions: 0
   }
-  return filesUnderAnalysis().reduce(calculateFileCoverage(overallCoverage), {metrics: []})
+  return Option(filesUnderAnalysis().reduce(calculateFileCoverage(overallCoverage), {metrics: []}))
 }
 
 /**
  * Formats the coverage metrics for display
  * @param coverageData
- * @notes CHANGES STATE
- * @returns {Array}
+ * @returns {*}
  */
 function formatCoverageMetrics (coverageData) {
   coverageData.reportData = coverageData.metrics.map(metric => [metric.file, formatDisplay(metric.coverage), metric.functions, sanityState(metric.coverage)])
@@ -43,23 +45,23 @@ function formatCoverageMetrics (coverageData) {
 }
 
 /**
- * Records coverage totals
- * @notes CHANGES STATE
- * @param coverageData
- */
-function recordCoverageTotals (coverageData) {
-  const totalPercent = coverageData.totalPercent
-  coverageData.reportData.push(['Total'.yellow, formatDisplay(totalPercent).yellow, coverageData.overallCoverage.functionCount, sanityState(totalPercent)])
-  return coverageData
-}
-
-/**
  * Calculates the total coverage for all files analyzed
- * @returns {number}
+ * @returns {*}
  */
 function calculateTotalCoverage(coverageData) {
   const coverageTotals = coverageData.overallCoverage
   coverageData.totalPercent = ((coverageTotals.dbcAssertions / 2) / coverageTotals.functionCount)
+  return coverageData
+}
+
+/**
+ * Records coverage totals
+ * @param coverageData
+ * @returns {*}
+ */
+function recordCoverageTotals (coverageData) {
+  const totalPercent = coverageData.totalPercent
+  coverageData.reportData.push(['Total'.yellow, formatDisplay(totalPercent).yellow, coverageData.overallCoverage.functionCount, sanityState(totalPercent)])
   return coverageData
 }
 
@@ -69,13 +71,6 @@ function calculateTotalCoverage(coverageData) {
  */
 function filesUnderAnalysis () {
   return glob.sync([config.glob])
-}
-
-/**
- * Writes out an indicator as files are processed
- */
-function writeProgress () {
-  process.stdout.write('.')
 }
 
 module.exports = rockford
